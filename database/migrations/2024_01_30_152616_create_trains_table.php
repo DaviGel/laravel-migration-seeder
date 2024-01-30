f469c9cb9c65c32838449c8d9d0112cc11e8edf2
DaviGel <geloso.davide@gmail.com>
2024-01-30 16:53:03 +0100

    feat: esercizio in progress

---
+++ database/migrations/2024_01_30_152616_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        Schema::table('trains', function (Blueprint $table) {
+            $table->string('azienda', 50)->nullable();
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->time('orario_di_partenza')->nullable();
+            $table->time('orario_di_arrivo')->nullable();
+            $table->date('data_di_partenza')->nullable();
+            $table->date('data_di_arrivo')->nullable();
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carrozze')->nullable();
+            $table->tinyInteger('in_orario')->default(1)->nullable();
+            $table->tinyInteger('cancellato')->default(0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
