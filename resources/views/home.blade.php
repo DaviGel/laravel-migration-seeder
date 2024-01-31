9c6423e5eb4286b8eb380d15cb2b155276956c3b
DaviGel <geloso.davide@gmail.com>
2024-01-31 11:33:35 +0100

    feat: modificato codice

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,13 @@
         <div class="container py-5">
             <ul class="list-unstyled">
                 @foreach ($trains as $train)
-                    <li>Partenza: {{ $train['stazione_di_partenza'] }} | Arrivo: {{ $train['stazione_di_arrivo'] }} | Data di partenza: {{ $train['data_di_partenza'] }} | Data di arrivo: {{ $train['data_di_arrivo'] }} | Codice treno: {{ $train['codice_treno'] }}</li>
+                    <li>
+                        Partenza: {{ $train->stazione_di_partenza }}
+                        | Arrivo: {{ $train->stazione_di_arrivo }}
+                        | Data di partenza: {{ $train->data_di_partenza }}
+                        | Data di arrivo: {{ $train->data_di_arrivo }}
+                        | Codice treno: {{ $train->codice_treno }}
+                    </li>
                 @endforeach
             </ul>
         </div>
